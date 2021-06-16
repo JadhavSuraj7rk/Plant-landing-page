a38901c6f070f4af6318c331171782738495f1cb
SURAJ JADHAV <69894599+JadhavSuraj7rk@users.noreply.github.com>
2021-06-16 12:26:58 +0530

    Update logic.js

--- js/logic.js
+++ js/logic.js
@@ -1,7 +1,9 @@
 //  Initialize Swiper 
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
-    spaceBetween: 30,
+    spaceBetween: 40,
+    centeredSlides: true,
+    grabCursor:true,
     loop: true,
     pagination: {
       el: ".swiper-pagination",
