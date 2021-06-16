d4b7732f3c1b7e09e2cfd54f20c6e639f430778d
SURAJ JADHAV <69894599+JadhavSuraj7rk@users.noreply.github.com>
2021-06-16 09:54:04 +0530

    Update index.html

---
+++ js/logic.js
@@ -0,0 +1,10 @@
+//  Initialize Swiper 
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
